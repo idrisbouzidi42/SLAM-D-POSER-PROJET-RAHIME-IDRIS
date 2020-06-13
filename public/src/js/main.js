de0b4c55a649f5a7d7877a4e6a886a8dd540227e
idrisbouzidi42 <bouzidi.idris42@gmail.com>
2020-06-13 12:05:05 +0200

    ajout du lien back to top

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,37 +1,29 @@
-/**
-* Template Name: Rapid - v2.0.0
-* Template URL: https://bootstrapmade.com/rapid-multipurpose-bootstrap-business-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-(function($) {
+(function ($) {
   "use strict";
 
-/* Navbar Scripts */
+  /* Navbar Scripts */
   // jQuery to collapse the navbar on scroll
-    $(window).on('scroll load', function() {
+  $(window).on('scroll load', function () {
     if ($(".navbar").offset().top > 20) {
       $(".fixed-top").addClass("top-nav-collapse");
     } else {
       $(".fixed-top").removeClass("top-nav-collapse");
     }
-    });
-
-  // jQuery for page scrolling feature - requires jQuery Easing plugin
-  $(function() {
-    $(document).on('click', 'a.page-scroll', function(event) {
-      var $anchor = $(this);
-      $('html, body').stop().animate({
-        scrollTop: $($anchor.attr('href')).offset().top
-      }, 600, 'easeInOutExpo');
-      event.preventDefault();
-    });
   });
 
-    // closes the responsive menu on menu item click
-    $(".navbar-nav li a").on("click", function(event) {
-    if (!$(this).parent().hasClass('dropdown'))
-        $(".navbar-collapse").collapse('hide');
-    });
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
 
 })(jQuery);
--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,43 +3,43 @@
 <section class="footer">
     <!-- footer-22 -->
     <div class="footer-hny py-5">
-        <div class="container py-lg-4"> 
-                    <div class="sub-columns row">
-                        <div class="sub-one-left col-lg-4 col-md-6">
-                            <h6>DEPOT PROJET </h6>
-                            <p>Proposer aux entreprises et étudiants du lycée Simone Weil, 
-                                localisé à Saint-Priest-en-Jarez, 
-                                une application web qui va leur permettre de poster des offres et demandes de stages.
-                            </p>
+        <div class="container py-lg-4">
+            <div class="sub-columns row">
+                <div class="sub-one-left col-lg-4 col-md-6">
+                    <h6>DEPOT PROJET </h6>
+                    <p>Proposer aux entreprises et étudiants du lycée Simone Weil,
+                        localisé à Saint-Priest-en-Jarez,
+                        une application web qui va leur permettre de poster des offres et demandes de stages.
+                    </p>
 
-                        </div>
-                        <div class="sub-two-right col-lg-4 col-md-6 my-md-0 my-5">
-                            <h6>Liens utiles</h6>
-                            <div class="footer-hny-ul">
-                            <ul>
-                                <li><a href="{{ url('/') }}">Accueil</a></li>
-                                <li><a href="{{ url('offres/index') }}">Les offres</a></li>
-                                <li><a href="{{ url('demandes/index') }}">Les demandes</a></li>
-                                <li><a href="{{ url('offres/create') }}">Poster une offre</a></li>
-                                <li><a href="{{ url('offres/demandes') }}">Poster une demande</a></li>
-                                <li><a href="{{ url('contact') }}">Contact</a></li>
-                            </ul>
-                            <ul>
-                                    <li><a href="#">Mentions Légales</a></li>
-                                    <li><a href="#">Politique de confidentialité</a></li>
-                            </ul>
-                           </div>
-                        </div>
-
-                        <div class="sub-one-left col-lg-4 col-md-6 mt-lg-0 mt-md-5">
-                            <h6>Lorem ipsum</h6>
-                        </div>
+                </div>
+                <div class="sub-two-right col-lg-4 col-md-6 my-md-0 my-5">
+                    <h6>Liens utiles</h6>
+                    <div class="footer-hny-ul">
+                        <ul>
+                            <li><a href="{{ url('/') }}">Accueil</a></li>
+                            <li><a href="{{ url('offres/index') }}">Les offres</a></li>
+                            <li><a href="{{ url('demandes/index') }}">Les demandes</a></li>
+                            <li><a href="{{ url('offres/create') }}">Poster une offre</a></li>
+                            <li><a href="{{ url('offres/demandes') }}">Poster une demande</a></li>
+                            <li><a href="{{ url('contact') }}">Contact</a></li>
+                        </ul>
+                        <ul>
+                            <li><a href="#">Mentions Légales</a></li>
+                            <li><a href="#">Politique de confidentialité</a></li>
+                        </ul>
                     </div>
                 </div>
-         </div>  
-        <div class="below-section">
-            <div class="container">
-                <div class="copyright-footer row">
+
+                <div class="sub-one-left col-lg-4 col-md-6 mt-lg-0 mt-md-5">
+                    <h6>Lorem ipsum</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="below-section">
+        <div class="container">
+            <div class="copyright-footer row">
                 <div class="columns col-lg-6">
                     <ul class="social footer">
                         <li><a href="#"><span class="fa fa-facebook" aria-hidden="true"></span></a></li>
@@ -51,27 +51,29 @@
                 </div>
                 <div class="columns copy-right col-lg-6">
                     <p>Ce site est la propriété du lycée Simone Weil.<br>
-                    © 2020 Tout droit réservé. Lycée Simone Weil, Saint-Priest-en-Jarez. Réaliser par 
-                    Idris Bouzidi et Rahime Soyiff étudiants BTS SIO.
-                </p>
+                        © 2020 Tout droit réservé. Lycée Simone Weil, Saint-Priest-en-Jarez. Réaliser par
+                        Idris Bouzidi et Rahime Soyiff étudiants BTS SIO.
+                    </p>
                 </div>
-            </div>
             </div>
         </div>
     </div>
+    </div>
 </section>
 
-  <!-- Bootstrap JqueryJS Files -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+<a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+<!-- Bootstrap JqueryJS Files -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
 
 
 
 
-  <!-- Template Main JS File -->
-    <script src="{{ asset('src/js/main.js') }}"></script>
-    <script src="{{ asset('src/js/validate.js') }}"></script>
+<!-- Template Main JS File -->
+<script src="{{ asset('src/js/main.js') }}"></script>
+<script src="{{ asset('src/js/validate.js') }}"></script>
 </body>
 
 </html>
