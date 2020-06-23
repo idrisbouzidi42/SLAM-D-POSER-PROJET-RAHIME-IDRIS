160a500394487fdb45b4b5172d64d89791926c9f
idrisbouzidi42 <bouzidi.idris42@gmail.com>
2020-06-23 12:45:22 +0200

    ajout page error et personalisation avec images

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@include('includes.head')
+<section class="pages-errors">
+    <div class="container">
+        <div class="error-content">
+            <img src="{{ asset('src/img/404.png') }}" alt="">
+            <h1>Oops ! Page non trouvée...</h1>
+            <a href="/">Retour ? </a>
+        </div>
+    </div>
+</section>
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,10 @@
+@include('includes.head')
+<section class="pages-errors">
+    <div class="container">
+        <div class="error-content">
+            <img src="{{ asset('src/img/500.png') }}" alt="">
+            <h1>Oops ! erreur serveur, un petit instant...</h1>
+            <a href="/">Retour ? </a>
+        </div>
+    </div>
+</section>
